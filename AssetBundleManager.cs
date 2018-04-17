1c26d7ca99591580b7d38fbfff9d644121f78ac1
Michael Pollind <mpollind@gmail.com>
2018-04-17 00:27:38 -0700

    added corkscrew coaster

--- AssetBundleManager.cs
+++ AssetBundleManager.cs
@@ -6,14 +6,20 @@ public class AssetBundleManager
     private Main Main {get;set;}
     private readonly Main _main;
     private AssetBundle assetBundle;
+
+    public readonly GameObject FrontCartGo;
+    public readonly GameObject CartGo;
+    public readonly GameObject SideCrossBeamsGo;
+
     public AssetBundleManager (Main main)
     {
         _main = main;
         var dsc = System.IO.Path.DirectorySeparatorChar;
-        assetBundle = AssetBundle.LoadFromFile( _main.Path + dsc + "assetbundle" + dsc + "reel");
+        assetBundle = AssetBundle.LoadFromFile( _main.Path + dsc + "assetbundle" + dsc + "assetpack");
 
-        //CartGo =  assetBundle.LoadAsset<GameObject> ("Cart");
-        //SideCrossBeamGo =  assetBundle.LoadAsset<GameObject> ("SideCrossBeams");
+        FrontCartGo =  assetBundle.LoadAsset<GameObject> ("01be2cec49bbb476381a537d75ad047e");
+        CartGo =  assetBundle.LoadAsset<GameObject> ("7c1045f838c59460db2bfebd3df04a47");
+        SideCrossBeamsGo =  assetBundle.LoadAsset<GameObject> ("c184c4f392587465f9bf2c86e6615e78");
     }
 
 }
--- Main.cs
+++ Main.cs
@@ -11,7 +11,6 @@ namespace CorkscrewCoaster
         private TrackRiderBinder binder;
 
         
-        public string Identifier { get; set; }
         GameObject _go;
         
         public void onEnabled()
@@ -20,10 +19,36 @@ namespace CorkscrewCoaster
             {
                 AssetBundleManager = new AssetBundleManager(this);
             }
-            binder = new TrackRiderBinder("da14b20fb34ccbeda5add956f8dde549");
+            binder = new TrackRiderBinder("kvwQwhKWWG");
             TrackedRide trackedRide = binder.RegisterTrackedRide<TrackedRide> ("Steel Coaster","CorkscrewCoaster", "Corkscrew Coaster");
-            CustomCoasterMeshGenerator trackGenerator = binder.RegisterMeshGenerator<CustomCoasterMeshGenerator> (trackedRide);
+            TwisterCoasterMeshGenerator trackGenerator = binder.RegisterMeshGenerator<TwisterCoasterMeshGenerator> (trackedRide);
             TrackRideHelper.PassMeshGeneratorProperties (TrackRideHelper.GetTrackedRide ("Steel Coaster").meshGenerator,trackedRide.meshGenerator);
+
+            trackGenerator.crossBeamGO = GameObjectHelper.SetUV (AssetBundleManager.SideCrossBeamsGo, 15, 14);
+
+            
+            trackedRide.price = 1200;
+            trackedRide.carTypes = new CoasterCarInstantiator[]{ };
+            trackedRide.meshGenerator.customColors = new[] { new Color(63f / 255f, 46f / 255f, 37f / 255f, 1), new Color(43f / 255f, 35f / 255f, 35f / 255f, 1), new Color(90f / 255f, 90f / 255f, 90f / 255f, 1) };
+            
+            
+            CoasterCarInstantiator coasterCarInstantiator = binder.RegisterCoasterCarInstaniator<CoasterCarInstantiator> (trackedRide, "CorkscrewCoasterInsantiator", "Corkscrew Car", 1, 10, 2);
+            
+            BaseCar frontCar =  binder.RegisterCar<BaseCar> ( AssetBundleManager.FrontCartGo,"CorkScrewCoaster_Front_Car", .35f,0f,true, new[] { 
+                new Color(71f / 255, 71f / 255, 71f / 255), 
+                new Color(176f / 255, 7f / 255, 7f / 255), 
+                new Color(26f / 255, 26f / 255, 26f / 255)}
+            );
+            coasterCarInstantiator.frontVehicleGO = frontCar;
+            coasterCarInstantiator.frontVehicleGO.gameObject.AddComponent<RestraintRotationController>().closedAngles = new Vector3(90,0,0);
+
+            BaseCar backCar =  binder.RegisterCar<BaseCar> ( AssetBundleManager.CartGo,"CorkScrewCoaster_Back_Car", .35f,-.3f,false, new[] { 
+                new Color(71f / 255, 71f / 255, 71f / 255), 
+                new Color(176f / 255, 7f / 255, 7f / 255), 
+                new Color(26f / 255, 26f / 255, 26f / 255)}
+            );
+            coasterCarInstantiator.vehicleGO = backCar;
+            coasterCarInstantiator.vehicleGO.gameObject.AddComponent<RestraintRotationController>().closedAngles = new Vector3(90,0,0);
 
             
             binder.Apply();
@@ -38,7 +63,7 @@ namespace CorkscrewCoaster
 
         public string Description => ".";
 
-        string IMod.Identifier => "VirginiaReel";
+        string IMod.Identifier => "CorkscrewCoaster";
 	
 	
         public string Path
